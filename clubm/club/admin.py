2be7db4c8f6323bc3433e8ef714ae3506a77f163
Deepak Kumar <86420333+Deepakkumark852@users.noreply.github.com>
2022-12-16 17:19:40 +0530

    Add files via upload

---
+++ clubm/club/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(club)
+admin.site.register(student)
+admin.site.register(event)
+admin.site.register(club_details)
+'''
+admin.site.register(request)
+admin.site.register(comments)'''
